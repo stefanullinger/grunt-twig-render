b89158a0baa770ca3ae725a024a4d1ff680451f9
Stefan Ullinger <stefan@slicemenice.de>
2014-01-28 22:10:18 +0100

    Defined twig_render task.

--- tasks/twig_render.js
+++ tasks/twig_render.js
@@ -10,41 +10,63 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var Twig = require("twig"),
+
+  DEFAULT_OPTIONS = {
+    // nothing here yet
+  };
+
+  function GruntTwigRender(options) {
+    this.options = options(DEFAULT_OPTIONS);
+  }
+
+  GruntTwigRender.prototype.render = function(data, template, dest) {
+    var data = this._getData(data);
+    
+    template = Twig.twig({
+      path: template,
+      async: false
+    });
+
+    grunt.file.write(dest, template.render(data));
+  }
+
+  GruntTwigRender.prototype._getData = function(data)
+  {
+    var datatype = typeof data;
+
+    if (datatype === "undefined" || data == null) {
+      grunt.fail.fatal("Data can not be undefined or null.");
+    }
+    else if (datatype === "string") {
+      return this._getDataFromFile(data);
+    }
+    else if (datatype !== "object") {
+      grunt.log.warn("Received data of type '" + datatype + "'. Expected 'object' or 'string'. Use at your own risk!");
+    }
+
+    return data;
+  }
+
+  GruntTwigRender.prototype._getDataFromFile = function(dataPath) {
+    if (/\.json/i.test(dataPath)) {
+      return grunt.file.readJSON(dataPath);
+    }
+    else {
+      grunt.log.warn("Data file does not seem to be JSON. Trying to evaluate the file's contents into an javascript object. Use at your own risk!");
+      return eval( '(' + grunt.file.read(dataPath) + ')' );
+    }
+  }
+
 
   grunt.registerMultiTask('twig_render', 'Render twig templates', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    
+    var renderer = new GruntTwigRender(this.options);
+
+    this.files.forEach(function(fileData) {
+      renderer.render(fileData.data, fileData.template, fileData.dest);
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
   });
 
 };
